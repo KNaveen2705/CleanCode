cdc272a73cce7b0590efa871f775ad872da9846f
KNaveen2705 <naveen.careers27@gmail.com>
2025-03-10 09:24:41 +0530

    Add files via upload

---
+++ DAY3/HamaraBasket.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace hamaraBasket
+{
+    public class HamaraBasket
+    {
+        IList<Item> Items;
+        public HamaraBasket(IList<Item> list)
+        {
+            this.Items = list;
+        }
+
+        public static HamaraBasket CreateHamaraBasket(IList<Item> list)
+        {
+            return new HamaraBasket(list);
+        }
+
+        public static IList<Item> CreateHamaraBasketList(string name, int sellIn, int quality)
+        {
+            Item item = new Item();
+            item.Name = name;
+            item.Quality = quality;
+            item.SellIn = sellIn;
+            IList<Item> myList = new List<Item>();
+            myList.Add(item);
+            return myList;
+        }
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Indian Wine" && Items[i].Name != "Movie Tickets")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Forest Honey")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Movie Tickets")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Forest Honey")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Indian Wine")
+                    {
+                        if (Items[i].Name != "Movie Tickets")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Forest Honey")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ DAY3/HamaraBasketTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace hamaraBasket
+{
+    [TestFixture]
+    public class HamaraBasketTest
+    {
+        HamaraBasket app;
+        IList<Item> items;
+        int sellIn = 10;
+        int quality = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            items = HamaraBasket.CreateHamaraBasketList("Apples", sellIn, quality);
+            app = HamaraBasket.CreateHamaraBasket(items);
+        }
+
+        [Test]
+        public void ShouldReduceSellInValueByOneEOD()
+        {
+            //Arrage
+            app.UpdateQuality();
+            Assert.That(items[0].SellIn, Is.EqualTo(sellIn - 1));
+        }
+
+        [Test]
+        public void ShouldReduceQualityValueByOneEOD()
+        {
+            //Arrage
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(quality - 1));
+        }
+    }
+}
